5b8f33e1e35d94cad289b3f7da0d774801d1a50e
brijp06 <137891484+brijp06@users.noreply.github.com>
2023-09-28 20:31:52 +0530

    Mainfirst

---
+++ WeightPavti/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbplease = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(194, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(143, 42)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Back Up"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lbplease
+        '
+        Me.lbplease.AutoSize = True
+        Me.lbplease.BackColor = System.Drawing.Color.Transparent
+        Me.lbplease.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbplease.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lbplease.Location = New System.Drawing.Point(210, 71)
+        Me.lbplease.Name = "lbplease"
+        Me.lbplease.Size = New System.Drawing.Size(185, 24)
+        Me.lbplease.TabIndex = 145
+        Me.lbplease.Text = "Please Wait..........."
+        Me.lbplease.Visible = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.BackgroundImage = Global.WeightPavti.My.Resources.Resources.final1
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Location = New System.Drawing.Point(3, 3)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(137, 98)
+        Me.Panel1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.BlanchedAlmond
+        Me.ClientSize = New System.Drawing.Size(407, 104)
+        Me.Controls.Add(Me.lbplease)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Cloud Backup"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbplease As Label
+End Class
---
+++ WeightPavti/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports Google.Apis.Auth
+Imports Google.Apis.Download
+Imports Google.Apis.Drive.v3
+Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Services
+Imports System.Threading
+Imports Google.Apis.Drive.v3.Data
+Imports Google.Apis.Upload
+Public Class Form1
+    'Private Service As DriveService = New DriveService
+    Dim clid As String
+    Dim authid As String
+    Dim path As String
+    Private moService As DriveService = New DriveService
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' readini()
+        CreateService()
+    End Sub
+    'Private Service As DriveService = New DriveService
+
+    Private Sub CreateService()
+        ' If Not BeGreen Then
+        Dim ClientId = "744206872529-useqh5d9sf0v412864jne6vj0fdi6e4b.apps.googleusercontent.com" 'clid
+        Dim ClientSecret = "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" 'authid
+        Dim MyUserCredential As UserCredential = GoogleWebAuthorizationBroker.AuthorizeAsync(New ClientSecrets() With {.ClientId = ClientId, .ClientSecret = ClientSecret}, {DriveService.Scope.Drive}, "user", CancellationToken.None).Result
+        moService = New DriveService(New BaseClientService.Initializer() With {.HttpClientInitializer = MyUserCredential, .ApplicationName = "Backup"})
+        path = Application.StartupPath & "\Backup\Mandli"
+        'End If
+    End Sub
+    Public Sub readini()
+        FileOpen(1, Application.StartupPath & "\autobackup.ini", OpenMode.Input)
+        Input(1, clid)
+        Input(1, authid)
+        Input(1, path)
+    End Sub
+    Private Sub UploadFile(FilePath As String)
+        'Me.Cursor = Cursors.WaitCursor
+        If moService.ApplicationName <> "Backup" Then CreateService()
+
+        Dim oGDriveFile As New File()
+
+        oGDriveFile.Name = “Backup” ' Set your File Name here.
+        oGDriveFile.Description = “Datebase Back Up” ' Set a meaningful description, I had set the same name as my project name
+        oGDriveFile.MimeType = “application/zip” ' You must set your MIME type carefully. Refer to the MIME table above
+
+        Dim bArrByteArray As Byte() = System.IO.File.ReadAllBytes(FilePath) ' Your File Path from where you would want to upload from.
+        Dim oStream As New System.IO.MemoryStream(bArrByteArray)
+
+        Dim oUploadRequest As FilesResource.CreateMediaUpload
+
+        oUploadRequest = moService.Files.Create(oGDriveFile, oStream, oGDriveFile.MimeType)
+
+        oUploadRequest.Fields = “id”
+        oUploadRequest.Alt = FilesResource.CreateMediaUpload.AltEnum.Json
+        ' Me.PictureBox1.Visible = True
+        lbplease.Visible = True
+
+        oUploadRequest.Upload() ' <- The file upload happens here.
+
+        Dim oFile As File = oUploadRequest.ResponseBody
+        '  Me.PictureBox1.Visible = False
+        lbplease.Visible = False
+
+        MsgBox("Upload Finished")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        path = path & Now.Day & Now.Month & Now.Year & ".Zip"
+        UploadFile(path)
+        'Me.PictureBox1.Visible = True
+        lbplease.Visible = True
+
+    End Sub
+
+
+End Class
